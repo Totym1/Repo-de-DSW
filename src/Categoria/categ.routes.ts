cb459222823f4f3fe0d4758a98e948e45bf04ce5
Pedro Nicolás <pnicolas@frro.utn.edu.ar>
2025-10-26 20:36:18 -0300

    Middleware para validar categoria

--- src/Categoria/categ.routes.ts
+++ src/Categoria/categ.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { CategoriaController } from "./categ.controller.js";
+import { validarCategoria } from "./categ.validate.js";
 
 const router = Router();
 const categoriaController = new CategoriaController();
 
-router.post('/categorias', categoriaController.createCategoria.bind(categoriaController));
+router.post('/categorias', validarCategoria, categoriaController.createCategoria.bind(categoriaController));
 router.get('/categorias', categoriaController.getAll.bind(categoriaController));
 router.get('/categorias/:_id', categoriaController.getCategoria.bind(categoriaController));
-router.put('/categorias/:_id', categoriaController.updateCategoria.bind(categoriaController));
+router.put('/categorias/:_id', validarCategoria, categoriaController.updateCategoria.bind(categoriaController));
 router.delete('/categorias/:_id', categoriaController.deleteCategoria.bind(categoriaController));
 
 export const categoriaRoutes = router;
---
+++ src/Categoria/categ.validate.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+
+// Middleware para validar y sanitizar
+export const validarCategoria = [
+  body("nombre")
+    .trim()                 // para sacar espacios al principio/final
+    .escape()               // convierte caracteres especiales como <, >, &, etc.
+    .notEmpty()             .withMessage("El nombre es obligatorio")
+    .isLength({ min: 2 })   .withMessage("El nombre debe tener al menos 2 caracteres")
+    .isString()             .withMessage("El nombre debe ser una cadena de texto"),
+
+  body("descripcion")
+    .trim()
+    .escape()
+    .notEmpty()             .withMessage("La descripción es obligatoria")
+    .isString()             .withMessage("La descripción debe ser una cadena de texto"),
+
+  (req:Request, res:Response, next:NextFunction) => {
+    const errores = validationResult(req);
+    if (!errores.isEmpty()) {
+      return res.status(400).json({ errors: errores.array() });
+    }
+    next();
+  },
+];
